23b59d31b3c8924fe365dd2de22fe0343b383775
cpf <cpfprogrammer@163.com>
2025-10-19 19:40:08 +0800

    简单修改，测试自动部署功能

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,18 +1,23 @@
 export default {
   title: "崔鹏飞的技术博客",
-  description: "分享 Java、Python、AI 学习笔记",
+  description: "分享 Java、Python、AI 技术笔记",
   themeConfig: {
-    nav: [
-      { text: "首页", link: "/" },
-      { text: "Java", link: "/java/intro" },
-      { text: "Python", link: "/python/basic" },
-      { text: "GitHub", link: "https://github.com/你的用户名/你的仓库" },
-    ],
+    logo: "/logo.png", // 这里填你放在 public 目录下的 logo 文件路径
     sidebar: {
       "/java/": [
-        { text: "Java 入门", link: "/java/intro" },
-        { text: "面向对象", link: "/java/oop" },
+        { text: "intro", link: "/java/01-intro.md" },
+        { text: "oop", link: "/java/02-oop.md" },
+        { text: "反射", link: "/java/03-反射.md" },
+      ],
+      "/python/": [
+        { text: "basic", link: "/python/01-python介绍.md" }
       ],
     },
+    nav: [
+      { text: "主页", link: "/" },
+      { text: "Java", link: "/java/01-intro.md" },
+      { text: "Python", link: "/python/01-python介绍.md" },
+      { text: "GitHub", link: "https://github.com/gitByCPF/my-blog" },
+    ],
   },
 }
